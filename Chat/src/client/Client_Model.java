99930573a435981b61accab5bc181cec9e7653aa
taehong kim <7538518@naver.com>
2017-03-03 11:23:31 +0900

    sign_up <업데이트 내용> 1. GUI 생성 	- Client : 'signup_frame' GUI 생성 2. DB 저장 기능 	- Client_Controller : 'create_userDB()' 생성 (Thread 사용) 	- Client_Model : 'client_ip' 변수 생성 (get_Method만 생성)
     * 주의사항 : Test할때, DB 계정 및 DB_table 자신이 만든 이름으로 바꾸고 사용할 것.
     			(table 이름 변경할때, 'create_userDB()' method 안에
     			'db_table(String)'변수 수정해서 사용할 것.)

--- Chat/src/client/Client_Model.java
+++ Chat/src/client/Client_Model.java
@@ -1,5 +1,22 @@
 package client;
 
+import java.net.InetAddress;
+
 public class Client_Model {
 
+	private String client_ip;
+
+	// set, get method
+	public String getClient_ip() {
+		String ip = null;
+		try {
+			InetAddress local = InetAddress.getLocalHost();
+			ip = local.getHostAddress();
+		} catch (Exception e) {
+			System.out.println("Error : Load Client_IP");
+		}
+		return ip;
+	}
+	
+	
 }
